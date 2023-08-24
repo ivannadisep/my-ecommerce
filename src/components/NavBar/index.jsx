632de0a1b4a1d9455fc7aeedb877622e360758ff
ivannadisep <103007168+ivannadisep@users.noreply.github.com>
2023-08-23 22:56:12 -0300

    segundo commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div className='container'>
+            <nav className='nav'>
+                <div className='nav_brand'>
+                    <a className='nav_link' href="a"> Mi Marca</a>
+                </div>
+                <ul className='nav_list'>
+                    <li>
+                        <a className='nav_link' href="a"> Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className='nav_link' href="a"> Categoria 2</a>
+                    </li>
+                    <li>
+                       <a className='nav_link' href="a"> Carrito</a> 
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default  NavBar
